827bc1271761fecdd3a941500c16a12683d487b6
khantkyaw6 <khantkaneki@gmail.com>
2025-05-17 01:48:22 +0700

    update for render

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const httpServer = http.createServer((req, res) => {
 	res.end("Hello, world!\n");
 });
 
+app.get("/", (req, res) => res.send("Hello from Render!"));
+
 httpServer.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
 });
